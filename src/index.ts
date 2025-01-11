e405a14e66e7a2bcd11f5fa97bba07f0e7fb7c1a
botagar <1773846+botagar@users.noreply.github.com>
2025-01-11 22:04:21 +1000

    introduce pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,57 @@
 import "dotenv/config";
-import { Client, Events, GatewayIntentBits, MessageFlags } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import events from "./events";
 import { deployCommands } from "./deploy-commands";
-import Groups from "./group/groups";
 import OnVoceStateUpdate from "./events/voiceStateUpdate";
 import { initIntervalJobs } from "./intervals";
+import pino from "pino";
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMembers,
-  ],
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
 });
+logger.info(`Starting LFG bot...`);
 
+logger.info(`Initializing Discord client...`);
+const discordClientIntents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildMembers,
+];
+if (process.env) {
+  const discordEnvVars = Object.keys(process.env)
+    .filter((key) => key.startsWith("DISCORD"))
+    .reduce((envVars, key) => {
+      envVars[key] = process.env[key];
+      return envVars;
+    }, {} as Record<string, string | undefined>);
+  logger.debug({
+    DISCORD_ENV_VARS: discordEnvVars,
+    DISCORD_CLIENT_INTENTS: discordClientIntents,
+  });
+}
+const client = new Client({
+  intents: discordClientIntents,
+});
+logger.info(`Discord client initialized`);
+
+logger.info(`Logging in to Discord...`);
 client.login(process.env.DISCORD_TOKEN);
+logger.info(`Logged in to Discord`);
 
 for (const event of events) {
+  logger.info({
+    msg: "Registering Discord event",
+    event: event,
+  });
   if (event.once) {
-    console.log(`Registering once event: ${event.name}`);
     client.once(event.name, (...args) => event.execute(...args));
   } else {
-    console.log(`Registering event: ${event.name}`);
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
 
 (async () => {
-  console.log("Deploying commands...");
-  if (process.env) {
-    console.log(
-      Object.keys(process.env)
-        .filter((key) => key.startsWith("DISCORD"))
-        .reduce((envVars, key) => {
-          envVars[key] = process.env[key];
-          return envVars;
-        }, {} as Record<string, string | undefined>)
-    );
-  }
+  logger.info("Deploying commands...");
   await deployCommands({ guildId: process.env.DISCORD_DEV_SERVER_ID! });
 })();
 
